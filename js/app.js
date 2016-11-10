3e4d708d60d548f4bcc6ebad3e80c62273f01f8b
Hitesh Kumar <hk.skit@hotmail.com>
2016-11-11 00:20:39 +0530

    Introduced new layout with About page.

---
+++ js/app.js
@@ -0,0 +1,36 @@
+/*global angular*/
+(function () {
+  'use strict';
+  var app = angular.module('codiary', [
+    'ui.router'
+  ]);
+  app.config([
+    '$urlRouterProvider',
+    '$stateProvider',
+    function ($urlRouterProvider, $stateProvider) {
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          template: '<h1>Home Page</h1>'
+        })
+        .state('about', {
+          url: '/about',
+          templateUrl: 'views/about.html'
+        })
+        .state('blog', {
+          url: '/blog',
+          template: '<h1>blog Page</h1>'
+        })
+        .state('other', {
+          url: '/about',
+          template: '<h1>Other Page</h1>'
+        })
+        .state('photos', {
+          url: '/photos',
+          template: '<h1>photos Page</h1>'
+        });
+      $urlRouterProvider.otherwise('/home');
+    }
+  ]);
+}());
--- modules/ngCodiaryCore.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  'use strict';
-  /**
-   * ngCodiaryCore plays the role of service layer. It is the one who is
-   * responsible for all the communications and api calls.
-   */
-  angular.module('ngCodiaryCore', []);
-}());
