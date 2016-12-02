d69426459776c10cb9e5089afd9b99720e588370
Hitesh Kumar <hk.skit@hotmail.com>
2016-12-02 12:46:01 +0530

    Remove spinner

--- js/modules/home/index.js
+++ js/modules/home/index.js
@@ -5,9 +5,7 @@
     ]);
   app.controller('HomeController', ['QuoteFactory', function (QuoteFactory) {
     var vm = this;
-
-    vm.loading = true;
-
+    
     function onLoad() {
       QuoteFactory.get()
         .then(function (quote) {
@@ -15,12 +13,9 @@
         })
         .catch(function (error) {
           vm.quote = {
-            quote: 'Something just blew up. Not able to fetch quote ',
+            quote: 'Something just blew up. Not able to fetch quote.',
             author: 'Codiary'
           };
-        })
-        .then(function () {
-          vm.loading = false;
         });
     }
 
