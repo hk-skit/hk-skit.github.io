1dc16961e3938d4e3391a6544cf9c76d54bd0724
Hitesh Kumar <hk.skit@gmail.com>
2017-03-02 23:03:50 +0530

    Added posts list

--- js/app.js
+++ js/app.js
@@ -7,15 +7,44 @@
 })();
 
 (function () {
-    /**
-     * MainController will we used as controller "as".
-     */
-    function MainController() {
-        this.greetings = 'Hola!';
+    'use strict';
 
+    function PostsFactory($http) {
+        var post_api = 'assets/json/posts.json';
+
+        function getPosts() {
+            return $http.get(post_api).then(function (response) {
+                return response.data;
+            }, function (error) {
+                return error;
+            });
+        }
+        return {
+            getPosts: getPosts
+        };
+    }
+    var app = angular.module('smellycode');
+    app.factory('PostsFactory', ['$http', PostsFactory])
+})();
+
+(function () {
+    /**
+     * MainController will we be used as controller "as".
+     */
+    function MainController($log, PostsFactory) {
+        var self = this;
+        self.posts = [];
+        PostsFactory.getPosts().then(function (posts) {
+            Array.prototype.push.apply(self.posts, posts);
+        }, function (error) {
+            //TODO: Notify user that something blew up..
+            $log.error(error);
+        });
     }
     var app = angular.module('smellycode');
     app.controller('MainController', [
+        '$log',
+        'PostsFactory',
         MainController
     ]);
 })();
