4bb0cbc21f3465cdf440d83316d532c0a7147432
Hitesh Kumar <hk.skit@gmail.com>
2018-09-04 09:01:15 +0530

    Introduced webpack.

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/scripts/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+};
