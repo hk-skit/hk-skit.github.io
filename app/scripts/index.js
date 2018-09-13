5ca7ab75e44445c20c5caae13be8037e3358aa49
Hitesh Kumar <hk.skit@gmail.com>
2018-09-13 11:45:44 +0530

    Added meta data for twitter

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,7 +3,7 @@ import '../styles/main.scss';
 import Plax from './plax';
 
 const layers = document.querySelectorAll('.plax');
-
 layers.forEach((layer) => Plax.plaxify(layer));
-
 Plax.enable();
+const dateYear = document.getElementById('date_year');
+dateYear.innerHTML = new Date().getFullYear();
