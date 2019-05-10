46ce7b1b2a4a46a8feb315f19f3953c326ffc426
Hitesh Kumar <hitesh.kumar2@myntra.com>
2019-05-10 23:50:32 +0530

    Added new content

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,8 +2,15 @@ import '../styles/main.scss';
 
 import Plax from './plax';
 
-const layers = document.querySelectorAll('.plax');
-layers.forEach((layer) => Plax.plaxify(layer));
-Plax.enable();
-const dateYear = document.getElementById('date_year');
-dateYear.innerHTML = new Date().getFullYear();
+window.onload = () => {
+  setTimeout(() => {
+    // To disable scroll restoration.
+    window.scrollTo(0, 0);
+
+    const layers = document.querySelectorAll('.plax');
+    layers.forEach(layer => Plax.plaxify(layer));
+    Plax.enable();
+    const dateYear = document.getElementById('date_year');
+    dateYear.innerHTML = new Date().getFullYear();
+  }, 50);
+};
