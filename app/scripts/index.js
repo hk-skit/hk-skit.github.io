5666bc1588816ab549884c7d5454aecbcef83d70
Hitesh Kumar <hitesh.kumar2@myntra.com>
2019-06-10 22:52:45 +0530

    Corrected open graph data

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,11 @@ import '../styles/main.scss';
 
 import Plax from './plax';
 
+import utils from './utils';
+
+// First day of career.
+const startDate = new Date(2014, 6, 15);
+
 window.onload = () => {
   setTimeout(() => {
     // To disable scroll restoration.
@@ -10,7 +15,11 @@ window.onload = () => {
     const layers = document.querySelectorAll('.plax');
     layers.forEach(layer => Plax.plaxify(layer));
     Plax.enable();
+
     const dateYear = document.getElementById('date_year');
     dateYear.innerHTML = new Date().getFullYear();
+
+    const totalExperience = document.getElementById('total_experience');
+    totalExperience.innerHTML = utils.getDiffInYears(new Date(), startDate);
   }, 50);
 };
--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -1,19 +1,27 @@
-const isUndefined = (obj) => obj === undefined;
+const isUndefined = obj => obj === undefined;
 
-const isNull = (obj) => obj === null;
+const isNull = obj => obj === null;
 
-const entries = (obj) => {
+const entries = obj => {
   if (Object.entries) {
     return Object.entries(obj);
   }
-  return Object.keys(obj).map((key) => [key, obj[key]]);
+  return Object.keys(obj).map(key => [key, obj[key]]);
 };
 
-const isString = (obj) => typeof obj === 'string';
+const isString = obj => typeof obj === 'string';
+
+const getDiffInYears = (dateA, dateB) => {
+  let diff = Math.abs(dateB.getTime() - dateA.getTime()); // diff in milliseconds.
+  diff /= 1000; // Convert to seconds
+  diff /= 60 * 60 * 24; // Convert to days
+  return Math.floor(diff / 365.25); // convert to years
+};
 
 export default {
   isUndefined,
   isNull,
   isString,
-  entries
+  entries,
+  getDiffInYears
 };
